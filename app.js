04272128a51b2e22b6e1f64bff3f7036f58b90e3
pranalipatil209 <pranalipatil209@gmail.com>
2016-12-20 11:31:27 +0530

    JSdoc comments added

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+/**
+ * Creating server instance & bind to the port
+ * Importing required modules
+ * @see module:app/config
+ **/
+
+/**
+ * import dependencies
+ * */
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
@@ -5,12 +14,18 @@ var express = require('express'),
 	config = require('./app/config'),
 	port = process.env.PORT || 3000;
 
+/** middleware */
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
-
 app.use(logger('dev'));
+
+/**
+ * using controllers as middleware
+ * @see module:app/controller
+ */
 app.use(require('./app/controller'));
 
+/** Assigning port to the server */
 app.listen(port,function(){
 	console.log('Server is running on port :: ',port);
 });
